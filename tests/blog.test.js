8c1936142baff61eecb722d9d9bcb1afa2cd867c
Marumo-ProG <Lennythobejane@gmail.com>
2024-03-30 08:27:08 +0200

    exercise 4.9

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -7,33 +7,29 @@ const app = require("../app");
 
 const api = supertest(app);
 
-// const initialContacts = [
-//   {
-//     name: "Tracy",
-//     number: "0715985514",
-//   },
-//   {
-//     name: "Boss Lady",
-//     number: "06319585651",
-//   },
-// ];
-
-beforeEach(async () => {
-  //   await Phone.deleteMany({});
-  //   let phoneObject = new Phone(initialContacts[0]);
-  //   await phoneObject.save();
-  //   phoneObject = new Phone(initialContacts[1]);
-  //   await phoneObject.save();
-});
-
 test("blogs returned are good", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
 
-  const response = await api.get("/api/blogs"); // getting the blogs to make sure the correct number of blogs is being returned
+  // getting the blogs to make sure the correct number of blogs is being returned
+  const response = await api.get("/api/blogs");
   assert.equal(response.body.length, 1);
+});
+
+test("unique identifier named id as oposed to _id", async () => {
+  // making sure that data is correct and being returned
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  // checking if all the data being returned has the key id and not _id
+  const response = await api.get("/api/blogs");
+  const number = response.body.filter((blog) => blog.id).length;
+
+  assert.equal(number, 1);
 });
 
 after(async () => {
